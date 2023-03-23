26cb95496327023148fa9724e07aeabd40dce889
itsDamon <nick.parise20@gmail.com>
2023-03-23 08:46:19 +0100

    Numero divisioni matrice a 5

--- main.py
+++ main.py
@@ -70,9 +70,9 @@ if __name__ == '__main__':
             mat = np.zeros((numeroDivisioniMatrice, numeroDivisioniMatrice))
             mask = filtro(im)  # chiama la funzione filtro e assegna il valore a mask
             for i in range(numeroDivisioniMatrice):
-                for j in range(numeroDivisioniMatrice + 1):
-                    crop = mask[MAXX // numeroDivisioniMatrice * j: MAXX // numeroDivisioniMatrice * (j + 1),
-                           MAXY // numeroDivisioniMatrice * i:MAXY // numeroDivisioniMatrice * (i + 1)]
+                for j in range(numeroDivisioniMatrice):
+                    crop = mask[MAXX // numeroDivisioniMatrice * j: (MAXX // numeroDivisioniMatrice-1) * (j + 1),
+                           MAXY // numeroDivisioniMatrice * i:(MAXY // numeroDivisioniMatrice-1) * (i + 1)]
                     mat[i][j] = isNero(crop, 30)
             print(mat)
             sleep(3)
