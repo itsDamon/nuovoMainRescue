b47e455865bf8ee894cd7930038bfb6e584a368f
itsDamon <nick.parise20@gmail.com>
2023-03-24 15:26:53 +0100

    diviso in 3 ogni quadrato per precisione decente

--- variabiliGlobali.py
+++ variabiliGlobali.py
@@ -2,4 +2,4 @@ MAXX = 640  # DIMENSIONE IMMAGINE X
 MAXY = 480  # DIMENSIONE IMMAGINE Y
 EXPOSURE = 35000  # ESPOSIZIONE CAMERA
 numeroDivisioniMatrice = 5
-SOGLIA = 600
+SOGLIA = (MAXX // numeroDivisioniMatrice * MAXY // numeroDivisioniMatrice)/3
