82a017706635b3b6584fed1ad9aec62a82543797
leor0 <lucascudeller05@gmail.com>
2023-03-31 17:09:39 +0200

    test matrice

--- test.py
+++ test.py
@@ -7,21 +7,23 @@ def oldDirezione(mat):
     startBase = numeroDivisioniMatrice-1 #inizio matrice dal basso
     startCentro = centroMatrice #centro iniziale
     while True:
-        if mat[startBase,startCentro] and startBase >= 0:
-            startBase +=1
-            continue
-        elif mat[startBase,startCentro-1] and startCentro >= 0 and startCentro <= 6:
-            startCentro -=1
-            continue
-        elif mat[startBase,startCentro] and startCentro <= 6 and startCentro >= 0:
-            startCentro +=1
-            continue
+        if startBase > -1 and startCentro > 0 and startCentro < 6:
+            if mat[startBase+1,startCentro]:
+                mat[startBase+1,startCentro] = 0
+                startBase +=1
+            else:
+                if mat[startBase,startCentro-1] :
+                    mat[startBase - 1, startCentro - 1] = 0
+                    startCentro -=1
+                elif mat[startBase,startCentro+1]:
+                    mat[startBase - 1, startCentro] = 0
+                    startCentro += 1
         if startCentro == centroMatrice:
-            return AVANTI
+            return AVANTI,mat
         elif startCentro > centroMatrice:
-            return DESTRA
+            return DESTRA,mat
         elif startCentro < centroMatrice:
-            return SINISTRA
+            return SINISTRA,mat
 
 
 mat = np.zeros((7, 7))
@@ -31,4 +33,5 @@ for i in range(6,2,-1):
 for j in range(3,7,+1):
     mat[3,j] = 1
 print(mat)
-print(oldDirezione(mat))
+a, b  = oldDirezione(mat)
+print(a,"\n", b)
