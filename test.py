ffd1f567478e73f9272adef988eefa1fd9bb6d12
StefanoB2018 <Stefanohu7@gmail.com>
2023-03-31 20:27:16 +0200

    Riduzione linee di codice

--- test.py
+++ test.py
@@ -10,35 +10,39 @@ def oldDirezione(mat):
     y = numeroDivisioniMatrice - 1  # inizio matrice dal basso
     x = centroMatrice  # centro iniziale
     # y-1 significa salire, x+1 destra, x-1 sinistra
-    while x<6 and y>0:
-        if mat[y - 1, x] == 1:  # controllo sopra di uno
-            y -= 1  # salgo di uno
-            mat[y, x] = 2  # assegno posizione attuale 2
-        elif mat[y - 1, x] == 0:
-            if mat[y, x - 1] == 1:
-                mat[y, x - 1] = 2
-                x -= 1
-            elif mat[y, x + 1] == 1:
-                mat[y, x + 1] = 2
-                x += 1
 
+    while 6 > x >=0 and y>0:
+        mat[y,x]= 0 #posizione iniziale a 0
+        if mat[y - 1, x]:  # controllo sopra di uno
+            y -= 1  # scorro sopra di uno
+        elif mat[y - 1, x] == 0: # se sopra 0
+            if mat[y, x - 1]: #controllo a sinistra
+                x -= 1 # scorro sinistra di uno
+            elif mat[y, x + 1]: #controllo a sinistra
+                x += 1 #scorro destra di uno
+        mat[y, x] = 2 # assegno posizione attuale 2
         print(mat)
         print("\n", 'x = ' + str(x), 'y = ' + str(y))
         sleep(1)
-        '''if x == centroMatrice:
-            return AVANTI, mat
-        elif x > centroMatrice:
-            return DESTRA, mat
-        elif x < centroMatrice:
-            return SINISTRA, mat'''
 
+    if x == centroMatrice:
+        return AVANTI, mat
+    elif x > centroMatrice:
+        return DESTRA, mat
+    elif x < centroMatrice:
+        return SINISTRA, mat
 
+#test purpose
+#genera linea di 1 nella matrice
 mat = np.zeros((7, 7))
-
 for i in range(6, 2, -1):
     mat[i, 3] = 1
-for j in range(3, 7, +1):
+#for j in range(3, 7, +1):
+for j in range(0, 4, +1):
     mat[3, j] = 1
-print(mat)
-a, b = oldDirezione(mat)
-print(a, "\n", b)
+
+#controllo matrice generato con gli 1
+print('\n',mat)
+a = oldDirezione(mat)
+#print risultato
+print('\n','return value:', a)
