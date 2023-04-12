bf1af3f55389323061d1766f66474e413a3ae0f2
StefanoB2018 <Stefanohu7@gmail.com>
2023-04-12 13:09:00 +0200

    More cases

--- test.py
+++ test.py
@@ -6,11 +6,16 @@ from variabiliGlobali import *
 
 
 def oldDirezione(mat):
+    checkX = 'init'
+    checkY = 'init'
     # bianco 1, startBase = Y, startCentro = X
     y = numeroDivisioniMatrice - 1  # inizio matrice dal basso
     x = centroMatrice  # centro iniziale
     # y-1 significa salire, x+1 destra, x-1 sinistra
     while 6 > x >=0 and y>0:
+        #if(x == checkX and y == checkY): #break se si ripete
+         #   print('\nRipetuto.')
+          #  break
         mat[y,x]= 0 #posizione iniziale a 0
         if mat[y - 1, x]:  # controllo sopra di uno
             y -= 1  # scorro sopra di uno
@@ -19,8 +24,10 @@ def oldDirezione(mat):
                 x -= 1 # scorro sinistra di uno
             elif mat[y, x + 1]: #controllo a sinistra
                 x += 1 #scorro destra di uno
-        print(mat)
-        print("\n", 'x = ' + str(x), 'y = ' + str(y))
+        checkX = x
+        checkY = y
+        print('\n',mat)
+        print('x =',x,',', 'y =',y)
         sleep(1)
         if x == 0 or y == 7 or x == 7 or y == 0:
             break
@@ -35,9 +42,10 @@ def oldDirezione(mat):
 #test purpose
 #genera linea di 1 nella matrice
 scelta = "Prova"
-while(scelta != 4):
+while(scelta != -1):
     mat = np.zeros((7, 7))
-    scelta = int(input("Test(4 exit): \n1.Avanti(return 3) \n2.Destra(return 2) \n3.Sinistra(return 1) \n-> "))
+    scelta = int(input("Test(4 exit / Return values Avanti 3 Destra 2 Sinistra 1): \n1.Linea dritta \n2.Curva Destra \n3.Curva Sinistra \n4.Forma + \n"
+                       "5.Linea dritta interrotta \n6.Diagonale Sinistra \n7.Diagonale Destra \n-> "))
     if(scelta == 1):
         for i in range(6):
             mat[i, 3] = 1
@@ -51,10 +59,36 @@ while(scelta != 4):
             mat[i, 3] = 1
         for j in range(0, 4, +1):
             mat[3, j] = 1
-    if(scelta != 4):
+    elif (scelta == 4):
+        for i in range(7):
+            mat[i, 3] = 1
+        for j in range(7):
+            mat[3, j] = 1
+    elif (scelta == 5):
+        for i in range(2,7):
+            mat[i, 3] = 1
+    elif (scelta == 6):
+        for i in range(4,7):
+            mat[i, 3] = 1
+        mat[5, 3] = 1
+        mat[3, 2] = 1
+        mat[4, 2] = 1
+        mat[3, 1] = 1
+        mat[2, 1] = 1
+        mat[2, 0] = 1
+    elif (scelta == 7):
+        for i in range(4,7):
+            mat[i, 3] = 1
+        mat[5, 3] = 1
+        mat[3, 4] = 1
+        mat[4, 4] = 1
+        mat[3, 5] = 1
+        mat[2, 5] = 1
+        mat[2, 6] = 1
+    if(scelta != -1):
         #controllo matrice generato con gli 1
-        print('\n',mat)
+        print('Start\n',mat,'\n')
         a, b = oldDirezione(mat)
         #print risultato
-        print('\n','return value:', a , "\n ", b , "\n")
+        print('return: ',a)
 print("Exit")
